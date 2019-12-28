231b046affb804b20f5018f081a4b886760658ec
Marek Witkowski <witkowski.marekk@gmail.com>
2019-12-28 19:35:06 +0100

    feat DI component

---
+++ bootstrap.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use App\Repository\LocationRepositoryInterface;
+use App\Repository\LocationRepository;
+use App\Entity\Location;
+
+require_once "vendor/autoload.php";
+
+/*
+ * Doctrine
+ */
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+$config = Setup::createAnnotationMetadataConfiguration(
+    array(__DIR__."/src/Entity"),
+    $isDevMode,
+    $proxyDir,
+    $cache,
+    $useSimpleAnnotationReader
+);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
+$classMetaData = $entityManager->getClassMetadata(Location::class);
+
+/*
+ * Dependency Injection
+ */
+$containerBuilder = new ContainerBuilder();
+
+/* register repository */
+$containerBuilder
+    ->register(LocationRepositoryInterface::class, LocationRepository::class)
+    ->addArgument($entityManager)
+    ->addArgument($classMetaData);
+/* register other services */
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
+$loader->load('services.yaml');
+$containerBuilder->compile();
